428c5271c3ebd6a55d2dae2fb733dcad60370f3a
DavidAlba <davidgalba@gmail.com>
2019-03-21 14:19:40 +0000

    EF Core setup + Unit testing (in memory)

---
+++ Catalog.API/Infrastructure/DatabaseContexts/MessagingContext.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Catalog.API.Infrastructure.DatabaseContexts
+{
+    public class MessagingContext : DbContext
+    {
+        public MessagingContext(DbContextOptions<MessagingContext> options) : base(options) { }
+
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Fluent API
+            //builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+        }
+    }
+
+    public class MessagingContextDesignFactory : IDesignTimeDbContextFactory<MessagingContext>
+    {
+        public MessagingContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=FutTalk.MessagesDataBase;Trusted_Connection=True;";
+            var optionsBuilder = new DbContextOptionsBuilder<MessagingContext>()
+                .UseSqlServer(
+                    connectionString, 
+                    options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(warning => warning.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new MessagingContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Catalog.API/Infrastructure/EntityConfigurations/MessageEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catalog.API.Infrastructure.EntityConfigurations
+{
+    public class MessageEntityTypeConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Message");
+
+            builder.Property(m => m.Id)
+                .ForSqlServerUseSequenceHiLo("message_hilo")
+                .IsRequired();
+
+            builder.Property(m => m.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+
+            builder.Property(m => m.Body)
+                .IsRequired(false);
+        }
+    }
+}
